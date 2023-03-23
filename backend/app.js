f2e6c43e10787e301810789f21514f2578090b07
alex <lapkesa@gmail.com>
2023-03-23 20:53:10 +0200

    try to back my prev commit(feat:add crash test from test#12)

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const rootRouter = require('./routes/index');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { cors } = require('./middlewares/cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+const { loginValidator, registrationValidator } = require('./utils/validators/usersValidator');
+
+const { PORT = 3000, DB_ADDRESS = 'mongodb://0.0.0.0:27017/mestodb' } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(requestLogger);
+app.use(helmet());
+mongoose
+  .connect(DB_ADDRESS)
+  .then(() => console.log('Соединение с базой данных установлено'))
+  .catch((err) => {
+    console.log(`Ошибка при подключении к базе данных: ${err.message}`);
+  });
+
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+app.post('/signin', loginValidator, login);
+app.post('/signup', registrationValidator, createUser);
+// Обработчики роутов для пользователей
+
+app.use(auth);
+app.use(rootRouter);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
